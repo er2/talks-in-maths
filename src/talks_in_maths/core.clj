e8261a1e0a5cd5b203a84a43017268c6a7f28917
Eric Riese <eric.riese@gmail.com>
2020-10-07 00:51:40 -0400

    web stuff added

--- src/talks_in_maths/core.clj
+++ src/talks_in_maths/core.clj
@@ -1,5 +1,7 @@
-(ns talks-in-maths.core)
-(require '[clojure.string :as str])
+(ns talks-in-maths.core
+  (:require [clojure.string :as str]
+         [compojure.core :refer :all]
+         [compojure.route :as route]))
 
 (defn split-ws
   "splits a string on whitespace"
@@ -10,3 +12,8 @@
   "Calculates the sequence of lengths of words in the given string"
   [txt]
   (map #(mod (.length %) 10) (split-ws txt)))
+
+(defroutes app
+           (GET "/" [] "<h1>Hello World</h1>")
+           (POST "/" params (pr-str (word-lengths (slurp (:body params)))))
+           (route/not-found "<h1>Page not found</h1>"))
